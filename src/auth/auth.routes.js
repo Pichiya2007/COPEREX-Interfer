a44e5ada4941e1074782620c8e7bb934363bcb18
Informatica <informatica@alumno.local>
2025-02-25 17:21:14 -0600

    Register, Login, CRUD Usuarios terminado, collection agregada, y validación de roles

---
+++ src/auth/auth.routes.js
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { login, register, updatePassword } from './auth.controller.js'
+import { registerValidator, loginValidator } from '../middlewares/validator.js';
+import { deleteFileOnError } from '../middlewares/delete-file-on-error.js';
+import { validarJWT } from '../middlewares/validar-jwt.js';
+import { validarCampos } from '../middlewares/validar-campos.js';
+
+const router = Router();
+
+router.post(
+    '/login',
+    loginValidator,
+    login
+);
+
+router.put(
+    '/updatepassword',
+    [
+        validarJWT,
+        validarCampos
+    ],
+    updatePassword
+);
+
+router.post(
+    '/register',
+    registerValidator,
+    deleteFileOnError,
+    register
+);
+
+export default router;
